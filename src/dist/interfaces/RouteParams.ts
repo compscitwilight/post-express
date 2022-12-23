c1d1a33ac9cca2fca8c3ead22eddae713f511d15
devrusty <rguillory246@gmail.com>
2022-12-23 06:04:07 -0600

    added RouteParams.misc

--- src/dist/interfaces/RouteParams.ts
+++ src/dist/interfaces/RouteParams.ts
@@ -1,10 +1,19 @@
 import HTTPMethod from "../types/HTTPMethod";
 import { RequestHandler } from "express";
+import PostExpressServer from "../classes/Server";
 
 interface RouteParams {
     path: string,
     method: HTTPMethod | HTTPMethod.GET,
-    handler: RequestHandler
+    handler: RequestHandler,
+    // RouteParams.misc
+    // NOTE: Everything in this property must be optional (ending with ?)
+    // otherwise, the changes may be breaking
+    // - devrusty (12-23-22)
+    misc?: {
+        redirect?: string,
+        originServer?: PostExpressServer
+    }
 }
 
 export default RouteParams;
