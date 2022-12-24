d895339d38ef0d1100a3eb5dc7c0bf535037ca70
devrusty <rguillory246@gmail.com>
2022-12-24 02:49:45 -0600

    changed exports

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,16 @@
 import PostExpressServer from "./dist/classes/Server";
 import RouteParams from "./dist/interfaces/RouteParams";
 import HTTPMethod from "./dist/types/HTTPMethod";
-import { StateConsole, StateConsoleState } from "./dist/utils/StateConsole";
-import PostExpressRoute from "./dist/classes/Route";
-import {Request, Response} from "express";
-import { generateRoute, generateRoutes, RouteGenerationParams } from "./dist/utils/generateRoute";
 import ExternalRequest from "./dist/classes/ExternalRequest";
+import PostExpressRoute from "./dist/classes/Route";
 
+export { StateConsole, StateConsoleState } from "./dist/utils/StateConsole";
+export {Request, Response} from "express";
+export { generateRoute, generateRoutes, RouteGenerationParams } from "./dist/utils/generateRoute";
 export {
     PostExpressServer,
     RouteParams,
     HTTPMethod,
-    StateConsole,
-    StateConsoleState,
-    Request,
-    Response,
     PostExpressRoute,
-    generateRoute,
-    generateRoutes,
-    RouteGenerationParams,
     ExternalRequest
 }
