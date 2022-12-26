7f217187125bd97f5ea94d767eeb527817683cd9
devrusty <rguillory246@gmail.com>
2022-12-25 22:59:35 -0600

    created Render

--- src/dist/classes/BetaServer.ts
+++ src/dist/classes/BetaServer.ts
@@ -4,11 +4,18 @@
  */
 
 import PostExpressServer from "./Server";
+import { Response } from "express";
+import { render } from "../../renderer";
+import RenderParameters from "../interfaces/RenderParameters";
 
 class PostExpressBetaServer {
     constructor(
         public readonly Server: PostExpressServer
     ) {}
+
+    public Render(res: Response, htmlRaw: string, renderParams?: RenderParameters[]) {
+        render(res, htmlRaw, renderParams);
+    }
 }
 
 export default PostExpressBetaServer;
